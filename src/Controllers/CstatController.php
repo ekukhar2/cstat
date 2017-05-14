2022597818fba8d12521cb343e8a67ced347a83c
Yevhen Kukhar <ekukhar2@gmail.com>
2017-05-14 15:38:47 +0300

    comment auth

--- src/Controllers/CstatController.php
+++ src/Controllers/CstatController.php
@@ -14,7 +14,7 @@ use Session;
 class CstatController extends Controller {
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
     public function index() {
         $cstat=new Cstat();
